dd25ccef997198b786f67fdc470c533858932e9d
daniellberg <danielbergons@gmail.com>
2023-09-03 22:11:43 -0300

    add renderPokemon

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImg = document.querySelector('.pokemon_image');
+
+const fetchPokemon = async (pokemon) => {
+    const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const data = await apiResponse.json();
+    return data;
+}
+
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+}
+
+renderPokemon('raichu');
