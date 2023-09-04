5645dd4c44c71f40622d3a279010c6dc47b470f0
daniellberg <danielbergons@gmail.com>
2023-09-03 22:44:35 -0300

    final

--- js/script.js
+++ js/script.js
@@ -2,17 +2,60 @@ const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImg = document.querySelector('.pokemon_image');
 
+const form = document.querySelector('.form');
+const input = document.querySelector('.search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let procurar = 1;
+
 const fetchPokemon = async (pokemon) => {
-    const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    const data = await apiResponse.json();
-    return data;
+    const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if(apiResponse.status === 200){
+        const data = await apiResponse.json();
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
+    
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemon(pokemon);
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+
+    if(data){
+        pokemonImg.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+        input.value = '';
+        procurar = data.id;
+    } else{
+        pokemonName.innerHTML = 'Not found :(';
+        pokemonNumber.innerHTML = '';
+        pokemonImg.style.display = 'none';
+    }
+    
 }
 
-renderPokemon('raichu');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+})
+
+buttonPrev.addEventListener('click', () => {
+    if(procurar >1){
+     procurar-=1;
+    renderPokemon(procurar);
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+   procurar+=1;
+   renderPokemon(procurar);
+});
+
+renderPokemon(procurar);
